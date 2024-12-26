f5902533a5097fd55ecc4de7cca480d44b3ac482
muhdfasil <muhammedfasiltk32745@gmail.com>
2024-12-26 20:51:59 +0530

    update

---
+++ 24_perfect_squre_even.py
@@ -0,0 +1,7 @@
+l=int(input("Enter lower limit:"))
+u=int(input("Enter upper limit:"))
+a=[x for x in range(l,u+1) if (int(x**0.5))**2==x]
+print("Perfect squeres are \n",a)
+a=[]
+for y in a:
+    pass
