b7d7e9d3e5f6b68d5a7acd815c88c61f976b4243
muhdfasil <muhammedfasiltk32745@gmail.com>
2025-01-05 13:35:40 +0530

    24

--- 24_perfect_squre_even.py
+++ 24_perfect_squre_even.py
@@ -1,7 +1,24 @@
-# l=int(input("Enter lower limit:"))
-# u=int(input("Enter upper limit:"))
-# a=[x for x in range(l,u+1) if (int(x**0.5))**2==x]
-# print("Perfect squeres are \n",a)
-# a=[]
-# for y in a:
-#     pass
+def digits_even(n):
+    for digit in str(n):
+        if int(digit) % 2 != 0:  
+            return False
+    return True
+
+def even_digit_squares(start, end):
+    result = []
+    
+    start_sqrt = int(start**0.5)  
+    end_sqrt = int(end**0.5)     
+    
+    for i in range(start_sqrt, end_sqrt + 1):  
+        square = i ** 2
+        if digits_even(square):           
+            result.append(square)
+    
+    return result
+
+start_range = int(input("Enter the start of the range: "))
+end_range = int(input("Enter the end of the range: "))
+
+result = even_digit_squares(start_range, end_range)
+print("Perfect squares with all even digits in the given range:", result)
